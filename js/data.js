d76cc7b4d518e3bc4fbc5866b8634060e74eab55
Rimantas <belauzas@yahoo.com>
2019-08-06 12:40:56 +0300

    Post rendering

--- js/data.js
+++ js/data.js
@@ -1,2 +1,135 @@
 "use strict";
 
+const posts = [
+    // {
+    //     author: {
+    //         id: 65131,
+    //         photo: 'person.jpg',
+    //         name: 'Name Surname'
+    //     },
+    //     time: 1540030365658,
+    //     content: {
+    //         text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+    //         photos: ['car-racing.png'],
+    //         background: 'red'
+    //     },
+    //     interactions: {
+    //         liked_person_ids: [65131, 584651, 54568, 46161, 9685316],
+    //         comment_ids: [15165, 8416, 5265]
+    //     }
+    // },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'John Doe'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit'
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Taylor Swift'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores exercitationem ipsa magni laudantium culpa fugiat optio aperiam corporis ipsum? Minus, optio sapiente dolore iste explicabo fugiat a maxime rerum. Accusamus? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores exercitationem ipsa magni laudantium culpa fugiat optio aperiam corporis ipsum? Minus, optio sapiente dolore iste explicabo fugiat a maxime rerum. Accusamus? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores exercitationem ipsa magni laudantium culpa fugiat optio aperiam corporis ipsum? Minus, optio sapiente dolore iste explicabo fugiat a maxime rerum. Accusamus?'
+        },
+        interactions: {
+            liked_person_ids: [65131, 584651]
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Name Surname'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+            photos: ['car-racing.png']
+        },
+        interactions: {
+            liked_person_ids: [65131],
+            comment_ids: [15165]
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Name Surname'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+            photos: ['portfolio.png', 'sign-up.png']
+        },
+        interactions: {
+            liked_person_ids: [65131, 584651, 54568, 46161, 9685316],
+            comment_ids: [15165, 8416, 5265]
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Name Surname'
+        },
+        time: 1540030365658,
+        content: {
+            photos: ['car-racing.png', 'portfolio.png', 'sign-up.png']
+        },
+        interactions: {
+            liked_person_ids: [65131, 584651, 54568, 46161, 9685316],
+            comment_ids: [15165, 8416, 5265]
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Name Surname'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
+            photos: ['car-racing.png', 'portfolio.png', 'sign-up.png', 'serlokas.png']
+        },
+        interactions: {
+            comment_ids: [15165, 8416, 5265]
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Name Surname'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet'
+        }
+    },
+    {
+        author: {
+            id: 65131,
+            photo: 'person.jpg',
+            name: 'Name Surname'
+        },
+        time: 1540030365658,
+        content: {
+            text: 'Lorem, ipsum dolor sit amet',
+            background: 'red'
+        },
+        interactions: {
+            liked_person_ids: [65131],
+            comment_ids: [15165, 8416]
+        }
+    }
+];
--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,81 @@
 "use strict";
 
+function generatePosts( data ) {
+    const target = document.querySelector('#feed');
+    let HTML = '';
+
+    // generuojame HTML: ciklas per duomenis -> HTML
+    data.forEach( post => {
+        HTML += `<div class="post">
+                    <header>
+                        <img src="./img/users/${post.author.photo}" alt="User photo">
+                        <div class="texts">
+                            <div class="title">${post.author.name}</div>
+                            <div class="time">${post.time} ago</div>
+                        </div>
+                        <i class="fa fa-ellipsis-h"></i>
+                    </header>
+                    ${ generateContent( post.content ) }
+                    <div class="interactions">
+                        ${ generateInteractions( post.interactions ) }
+                    </div>
+                    <div class="comment-form">
+                        <img src="#" alt="My photo">
+                        <div class="form">
+                            <textarea></textarea>
+                            <div class="icons">
+                                <i class="fa fa-globe"></i>
+                                <i class="fa fa-globe"></i>
+                                <i class="fa fa-globe"></i>
+                                <i class="fa fa-globe"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    });
+
+    // iklijuojame sugeneruota HTML
+    target.insertAdjacentHTML('beforeend', HTML);
+
+    // sudedame ant nauju post'u event listener
+    //....
+
+    return;
+}
+
+function generateInteractions( data ) {
+    let HTML = '',
+        likes = 0,
+        comments = 0;
+    
+    if ( data !== undefined ) {
+        if ( data.liked_person_ids !== undefined ) {
+            likes = data.liked_person_ids.length;
+        }
+        if ( data.comment_ids !== undefined ) {
+            comments = data.comment_ids.length;
+        }
+    }
+    
+    HTML = `<div class="action">
+                <i class="fa fa-thumbs-o-up"></i>
+                <span>Like${likes > 0 ? ` (${likes})` : ''}</span>
+            </div>
+            <div class="action">
+                <i class="fa fa-comment-o"></i>
+                <span>Comment${comments > 0 ? ` (${comments})` : ''}</span>
+            </div>`;
+
+    return HTML;
+}
+
+function generateContent( data ) {
+    let HTML = '';
+
+    HTML = `<div class="content">
+                ${ typeof(data.text) === 'string' ? `<p>${data.text}</p>` : '' }
+                NUOTRAUKOS
+            </div>`;
+
+    return HTML;
+}
